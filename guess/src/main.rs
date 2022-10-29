28b50e3366d79bdffb466d413cd56911e5942e0c
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-10-29 21:17:19 +0800

    guess

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -4,22 +4,32 @@ use std::io;
 use rand::Rng; // trait
 
 fn main() {
-    println!("猜数!");
-    println!("猜测一个数");
-
     let i: i32 = rand::thread_rng().gen_range(1, 101);
-    println!("生成的数字是:{}", i);
+    // println!("生成的数字是:{}", i);
 
-    let mut guess = String::new();
 
-    let _ = io::stdin().read_line(&mut guess).expect("无法读取行");
+    loop {
+        println!("猜测一个数");
+        let mut guess: String = String::new();
 
-    println!("你猜测的数是:{}", guess);
-    let i1 = guess.trim().parse::<i32>().expect("error");
+        let _ = io::stdin().read_line(&mut guess).expect("无法读取行");
 
-    match i1.cmp(&i) {
-        Ordering::Equal => println!("You Win!"),
-        Ordering::Greater => println!("Too small!"),
-        Ordering::Less => println!("To bigger!")
+        println!("你猜测的数是:{}", guess);
+
+        let i1: i32 = match guess.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        match i1.cmp(&i) {
+            Ordering::Equal =>
+                {
+                    println!("You Win!");
+                    break;
+                }
+            Ordering::Greater => println!("Too Bigger!"),
+            Ordering::Less => println!("To Smaller!")
+        }
     }
 }
