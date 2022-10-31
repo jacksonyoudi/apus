4b1be8effba6e47179b9a8e47ba09c762d28c78c
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-10-31 21:51:13 +0800

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,4 +7,20 @@ fn main() {
     println!("{:?}", x);
 
     println!("Hello, world!");
+
+
+
+}
+
+
+fn first_word(s: &String) -> usize {
+    let x: &[u8] = s.as_bytes();
+
+
+    for (i, &item) in x.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
