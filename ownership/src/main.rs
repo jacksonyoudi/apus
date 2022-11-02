abb6cf1268d9dc1b03d9729b61a12d6b0179540a
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-11-02 22:55:50 +0800

    basisc

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,5 @@
 use macroadvance;
+use std::ops::Drop;
 
 fn main() {
     let x = macroadvance::my_vec![1,2,3];
@@ -7,6 +8,17 @@ fn main() {
     println!("{:?}", x);
 
     println!("Hello, world!");
+
+    let s: S = S(1);
+    println!("{:?}", s);
+
+    let s: S = S(2);
+    println!("{:?}", s);
+
+    let a: A = A { a: 1, b: 2 };
+    let b: A = a;
+
+    println!("{:?}", a);
 }
 
 
@@ -39,6 +51,29 @@ impl<T> Point<T> {
 
 
 // 单态化静态分发的好处是性能好，没有运行时开销；缺点是容易造成编译后生成的二进制文件膨胀。
+
+
+// SBRM 作用域界定资源管理
+#[derive(Debug)]
+struct S(i32);
+
+impl Drop for S {
+    fn drop(&mut self) {
+        println!("drop {}", self.0)
+    }
+}
+
+
+#[derive(Debug, Copy, Clone)]
+struct A {
+    a: u32,
+    b: u32,
+}
+
+
+
+
+
 
 
 
