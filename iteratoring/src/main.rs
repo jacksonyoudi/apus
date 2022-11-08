7b42892d6a86077446266ba1a86bb04dd80e4f2b
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-11-08 20:29:21 +0800

    iteratoring

--- iteratoring/src/main.rs
+++ iteratoring/src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut counter: Counter = Counter { count: 0 };
+    println!("{}", counter.next().unwrap());
+    println!("{}", counter.next().unwrap());
 }
+
+
+// trait InIterator<T: Copy> {
+//     fn each<F: Fn(T) -> T>(&mut self, f: F);
+// }
+//
+// impl<T: Copy> InIterator<T> for Vec<T> {
+//     fn each<F: Fn(T) -> T>(&mut self, f: F) {
+//         let mut i: i32 = 0;
+//         let x: i32 = self.len().try_into().unwrap();
+//         while i < x {
+//             self[i] = f(self[i]);
+//             i += 1;
+//         }
+//     }
+// }
+
+
+struct Counter {
+    count: usize,
+}
+
+
+impl  Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+
+
+
+
