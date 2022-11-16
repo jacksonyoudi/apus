c87d89393a8a8350da9bc2cb604474111ced18cd
youdi <liangchangyoujackson@gmail.com>
2022-11-16 10:08:25 +0800

    thread

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,5 +1,6 @@
 use std::cmp::Ordering;
 use std::{io, thread};
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 // prelude
 use rand::Rng; // trait
@@ -55,9 +56,19 @@ fn main() {
     park_thread.join().unwrap();
 
 
+    let s = Arc::new(Mutex::new("hello".to_string()));
+    let mut v = vec![];
 
-    
-
-
+    for _ in 0..3 {
+        let s_clone = s.clone();
+        let child = thread::spawn(move || {
+            let mut s_clone = s_clone.lock().unwrap();
+            s_clone.push_str(" world!");
+        });
+        v.push(child);
+    }
+    for child in v {
+        child.join().unwrap();
+    }
 }
 
